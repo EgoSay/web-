823008af3065dd564bd66e7e53cf5727abdea7e5
low-key-Tony <961663316@qq.com>
2018-06-14 20:13:36 +0800

    springmvc学习记录2: 1.使用ResponseBody注解进行数据交互会出现415错误，原因是json对象无法转化为实体类对象 2.处理Ajax跨域传递json数据时出现中文乱码

--- src/main/java/com/cjw/test/controller/ItemsController.java
+++ src/main/java/com/cjw/test/controller/ItemsController.java
@@ -1,20 +1,21 @@
 package com.cjw.test.controller;
 
 import com.cjw.test.po.Items;
-import com.cjw.test.po.QueryVo;
+import com.cjw.test.pojo.MyException;
+import com.cjw.test.pojo.QueryVo;
 import com.cjw.test.service.ItemServices;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -29,6 +30,10 @@ public class ItemsController {
 
     @RequestMapping(value = "/itemList.action")
     public ModelAndView itemList() {
+//        Integer i = 1/0;
+//        if(null == null){
+//			throw new MyException("商品信息不能为空");
+//		}
         List<Items> items = itemServices.queryItems();
         // 创建ModelAndView，用来存放数据和视图
         ModelAndView modelAndView = new ModelAndView();
@@ -72,6 +77,7 @@ public class ItemsController {
      */
     @RequestMapping("/itemEdit")
     public String toItemsEdit(Integer id, ModelMap model){
+
         Items items = itemServices.queryItemsById(id);
         model.addAttribute("item",items);
         return "editItem";
@@ -85,7 +91,7 @@ public class ItemsController {
     public String updateItem(QueryVo vo){
 
         itemServices.updateItemById(vo.getItems());
-
+        /*重定向，数据会丢失，浏览器地址变化*/
         return "redirect:/itemList.action";
     }
     /**
@@ -95,6 +101,34 @@ public class ItemsController {
     public String deleteItemById(Integer[] ids){
 
         return "success";
+    }
+
+    /**
+     * 批量修改，包装类型vo
+     */
+    @RequestMapping("/updateItems")
+    public String updateItems(QueryVo vo) {
+        return "success";
+    }
+
+    /**
+     * 处理json数据
+     * 1.使用ResponseBody注解进行数据交互会出现415错误，原因是json对象无法转化为实体类对象
+     * 2.处理Ajax跨域传递json数据时，当服务器端返回数据为String类型时出现中文乱码
+     */
+
+    @RequestMapping("/json.action")
+    @ResponseBody
+    public void json(@RequestBody String str, HttpServletResponse response) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Items items =  objectMapper.readValue(str, Items.class);
+        System.out.println(items);
+        Gson gson = new Gson();
+        String json = gson.toJson(items);
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(json);
+        //return json;
     }
 }
 
